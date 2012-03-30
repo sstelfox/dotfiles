531492efb5faaf4697c26b10d85a9bd8ba324c0c
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2012-03-30 10:33:40 -0400

    Added a default lifetime of 1 hour on all keys add to the SSH agent

--- helpers/ssh-agent.sh
+++ helpers/ssh-agent.sh
@@ -6,7 +6,7 @@ SSH_ENV="$HOME/.ssh/environment"
 function start_agent {
     echo "Initializing new SSH agent..."
     # spawn ssh-agent
-    ssh-agent | sed 's/^echo/#echo/' > "$SSH_ENV"
+    ssh-agent -t 3600 | sed 's/^echo/#echo/' > "$SSH_ENV"
     echo succeeded
     chmod 600 "$SSH_ENV"
     . "$SSH_ENV" > /dev/null
