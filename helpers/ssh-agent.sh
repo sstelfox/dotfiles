d914f4b7a071f40cdd8d4e4731c8b93dd7055bc8
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2012-04-04 11:04:49 -0400

    Increased SSH identity timeout to four hours

--- helpers/ssh-agent.sh
+++ helpers/ssh-agent.sh
@@ -6,7 +6,7 @@ SSH_ENV="$HOME/.ssh/environment"
 function start_agent {
     echo "Initializing new SSH agent..."
     # spawn ssh-agent
-    ssh-agent -t 3600 | sed 's/^echo/#echo/' > "$SSH_ENV"
+    ssh-agent -t 14400 | sed 's/^echo/#echo/' > "$SSH_ENV"
     echo succeeded
     chmod 600 "$SSH_ENV"
     . "$SSH_ENV" > /dev/null
