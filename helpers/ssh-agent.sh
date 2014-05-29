7090baf039b1896a07d05cf909836cf32f0f3c67
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-05-28 21:30:19 -0400

    Tweaks too the ssh-agent system to make it more robust

--- helpers/ssh-agent.sh
+++ helpers/ssh-agent.sh
@@ -1,70 +1,103 @@
 #!/bin/bash
 
-function error_handler() {
-  echo "$(basename ${BASH_SOURCE[0]}) Error occurred in script at line ${1} with status code ${2}"
-  set +o errtrace
-}
-
-trap 'error_handler ${LINENO} $?' ERR
-
-set -o errtrace
-
+# Expiration in seconds that the SSH agent will hold on to the keys.
+AGENT_TIMEOUT=14400
 SSH_ENV="$HOME/.ssh/environment"
 
-function destroy_gnome_interference {
-  if [ -n "${GNOME_KEYRING_PID}" ]; then
-    if $(kill -0 ${GNOME_KEYRING_PID} &> /dev/null); then
-      kill -9 ${GNOME_KEYRING_PID}
+# If the environment file exists, source it. If it's not accurate it'll be
+# dealt with later.
+if [ -f "${SSH_ENV}" ]; then
+  . ${SSH_ENV}
+fi
+
+# Remove the error tracing and unset our trap.
+function cleanup() {
+  set +o errtrace
+  trap - ERR
+}
+
+# A simple error handler that allows us to trace back any issues encountered
+# within a script back too their source.
+function error_handler() {
+  echo
+  echo "$(basename ${1}) Error occurred in script at line ${2} with status code ${3}"
+  echo "Relevant line was: $(sed -n ${2}p ${1})"
+
+  # Only one error can occur in a script before it exits, so be sure we
+  # cleanup after ourselves.
+  cleanup
+}
+
+# Setup the error handler as early as possible
+trap 'error_handler "${BASH_SOURCE[0]}" ${LINENO} $?' ERR
+set -o errtrace
+
+# Return whether or not there is a valid agent running
+function check_agent_status() {
+  if $(ps h ${SSH_AGENT_PID}); then
+    # Agent's PID still seems active... Can we check the status of our keys?
+    if $(ssh-add -l &> /dev/null); then
+      # We still have an active agent connection
+      return 0
     fi
   fi
 
-  unset GNOME_KEYRING_CONTROL SSH_AUTH_SOCK GNOME_KEYRING_PID
+  if [ -n "${SSH_AUTH_SOCK}" ]; then
+    rm -f ${SSH_AUTH_SOCK}
+  fi
+
+  unset SSH_AGENT_PID SSH_AUTH_SOCK
+  return 1
+}
+
+# Gnome is a damn evil bastard, it tries wants control over SSH agent's but
+# I'll have none of that.
+function destroy_gnome_interference() {
+  if [ -n "${GNOME_KEYRING_PID}" ]; then
+    echo -n "Detected Gnome's keyring... "
+    if $(ps h ${GNOME_KEYRING_PID}); then
+      echo -n "And it's active... "
+      kill -9 ${GNOME_KEYRING_PID}
+      echo "Now it's not."
+    else
+      echo "But it's dead..."
+    fi
+  fi
+
+  unset GNOME_KEYRING_CONTROL GNOME_KEYRING_PID SSH_ASKPASS
 }
 
 # start the ssh-agent
 function start_agent {
   echo "Initializing new SSH agent..."
   # spawn ssh-agent
-  ssh-agent -t 14400 | sed 's/^echo/#echo/' > "$SSH_ENV"
-  echo succeeded
-  chmod 600 "$SSH_ENV"
-  . "$SSH_ENV" > /dev/null
-  ssh-add
+  if $(ssh-agent -t 14400 | sed 's/^echo/#echo/' > "${SSH_ENV}"); then
+    echo "Succeeded"
+    chmod 0600 "$SSH_ENV"
+    . "$SSH_ENV" > /dev/null
+    ssh-add
+  fi
 }
 
-# test for identities
 function test_identities {
-  # test whether standard identities have been added to the agent already
-  ssh-add -l | grep "The agent has no identities" > /dev/null
-  if [ $? -eq 0 ]; then
+  # Test whether standard identities have been added to the agent already
+  if $(ssh-add -l | grep "The agent has no identities" > /dev/null); then
     ssh-add
-    # $SSH_AUTH_SOCK broken so we start a new proper agent
-    if [ $? -eq 2 ];then
-      start_agent
-    fi
   fi
 }
 
 destroy_gnome_interference
 
-# check for running ssh-agent with proper $SSH_AGENT_PID
-if [ -n "$SSH_AGENT_PID" ]; then
-  ps -ef | grep "$SSH_AGENT_PID" | grep ssh-agent > /dev/null
-  if [ $? -eq 0 ]; then
-    test_identities
-  fi
-# if $SSH_AGENT_PID is not properly set, we might be able to load one from
-# $SSH_ENV
+# If no agent...
+if check_agent_status; then
+  echo "Agent seems good..."
 else
-  if [ -f "$SSH_ENV" ]; then
-    . "$SSH_ENV" > /dev/null
-  fi
-  ps -ef | grep "$SSH_AGENT_PID" | grep -v grep | grep ssh-agent > /dev/null
-  if [ $? -eq 0 ]; then
-    test_identities
-  else
-    start_agent
-  fi
+  echo "Need too startup the agent..."
+  start_agent
 fi
 
-set +o errtrace
+test_identities
+
+# Once the script has exited we need to ensure we cleanup our changed settings.
+cleanup
+
