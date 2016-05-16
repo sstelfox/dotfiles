daaf37ac7b69dbb371b2c73f42b6fef02efea115
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2016-05-16 10:30:40 -0400

    Added a small script for tracking my system volume changes... WTF is going on

---
+++ scripts/volume_watch.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+VOL_TRACK_FILE="$HOME/.volume_watch"
+VOL_CHANGE_LOG="$HOME/volume_change.log"
+
+function getCurrentVolume {
+  amixer -D pulse get Master | grep -oE '[[:digit:]]+%' | sort -u
+}
+
+function getLastVolume {
+  if [ -f "${VOL_TRACK_FILE}" ]; then
+    cat ${VOL_TRACK_FILE}
+  fi
+}
+
+LST_VOL="$(getLastVolume)"
+CUR_VOL="$(getCurrentVolume)"
+
+echo ${CUR_VOL} > ${VOL_TRACK_FILE}
+
+if [ -n "${LST_VOL}" -a "${CUR_VOL}" != "${LST_VOL}" ]; then
+  MSG="Volume changed from ${LST_VOL} to ${CUR_VOL}"
+  echo "[$(date -Iseconds)] ${MSG}" >> ${VOL_CHANGE_LOG}
+  notify-send 'Volume Change' "${MSG}"
+fi
