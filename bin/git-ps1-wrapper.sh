748120bda4fec951e84e3903b88b0e50d986116d
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2017-10-16 11:07:19 -0400

    Handle the alternate location of the git setup prompt script

--- bin/git-ps1-wrapper.sh
+++ bin/git-ps1-wrapper.sh
@@ -6,3 +6,11 @@ if [ -f $GIT_PROMPT_LOC ]; then
   source $GIT_PROMPT_LOC
   __git_ps1
 fi
+
+# Arch Linux stores this in a different location
+ALT_PROMPT_LOC="/usr/share/git/completion/git-prompt.sh"
+
+if [ -f $ALT_PROMPT_LOC ]; then
+  source $ALT_PROMPT_LOC
+  __git_ps1
+fi
