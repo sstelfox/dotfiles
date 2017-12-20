96e1a19680e498f40f7864135ec7ebfbe68bc92e
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2017-12-20 11:54:33 -0500

    Yeah I really do want to use my options as default, other people using my repo can make this change

--- system-specific/git-user-info.sh
+++ system-specific/git-user-info.sh
@@ -4,8 +4,8 @@
 # gitconfig file which is useful because the gitconfig file can't
 # source other files, and these can be system specific between say
 # work and home machines.
-export GIT_COMMITTER_EMAIL=""
-export GIT_COMMITTER_NAME=""
+export GIT_COMMITTER_EMAIL="sstelfox@bedroomprogrammers.net"
+export GIT_COMMITTER_NAME="Sam Stelfox"
 
 # You probably don't need to change these unless they're different
 # for some reason than those above
