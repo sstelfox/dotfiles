e67776d2a48ae1991cac0e8871931cef2ae86f18
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-04-07 19:16:25 -0400

    Add cinnamon config, clean up install script, comment out nftables setup, add in separate installation script for nvidia drivers

--- fresh_setup.sh
+++ fresh_setup.sh
@@ -13,57 +13,8 @@ sudo dnf install arm-none-eabi-gdb awscli docker docker-compose fswebcam gdb \
 
 sudo systemctl start pcscd.service
 sudo systemctl enable pcscd.service
-sudo systemctl disable firewalld.service
-sudo systemctl mask firewalld.service
 sudo systemctl enable sshd.service
 sudo systemctl start sshd.service
-
-cat << 'EOF' | sudo tee /etc/sysconfig/nftables.conf
-flush ruleset
-
-table inet filter {
-  chain input {
-    type filter hook input priority 0; policy drop;
-
-    ct state invalid drop
-    ct state established,related accept
-    iif "lo" accept
-
-    ip protocol icmp accept
-    ip6 nexthdr icmpv6 accept
-
-    tcp dport 22 accept
-
-    ip protocol tcp tcp dport 67 tcp sport 68 accept
-
-    counter
-  }
-
-  chain forward {
-    type filter hook forward priority 0; policy drop;
-  }
-
-  chain output {
-    type filter hook output priority 0; policy drop;
-
-    ct state established,related accept
-    oif "lo" accept
-
-    ip protocol icmp accept
-    ip6 nexthdr icmpv6 accept
-
-    tcp dport 67 tcp sport 68 accept
-    tcp dport { 53, 80, 443, 873, 2200 } accept
-    udp dport 53 accept
-
-    ct state new log level warn prefix "egress attempt: "
-    counter reject with icmp type admin-prohibited
-  }
-}
-EOF
-
-sudo systemctl enable nftables.service
-sudo systemctl start nftables.service
 
 sudo groupadd -r docker
 sudo usermod -aG dialout,docker $(whoami)
@@ -97,3 +48,53 @@ sudo dnf install patch autoconf automake bison gcc-c++ libffi-devel libtool \
 rvm install ruby-2.6.1
 
 echo 'You probably still need a reboot...'
+
+#sudo systemctl disable firewalld.service
+#sudo systemctl mask firewalld.service
+#
+#cat << 'EOF' | sudo tee /etc/sysconfig/nftables.conf
+#flush ruleset
+#
+#table inet filter {
+#  chain input {
+#    type filter hook input priority 0; policy drop;
+#
+#    ct state invalid drop
+#    ct state established,related accept
+#    iif "lo" accept
+#
+#    ip protocol icmp accept
+#    ip6 nexthdr icmpv6 accept
+#
+#    tcp dport 22 accept
+#
+#    ip protocol tcp tcp dport 67 tcp sport 68 accept
+#
+#    counter
+#  }
+#
+#  chain forward {
+#    type filter hook forward priority 0; policy drop;
+#  }
+#
+#  chain output {
+#    type filter hook output priority 0; policy drop;
+#
+#    ct state established,related accept
+#    oif "lo" accept
+#
+#    ip protocol icmp accept
+#    ip6 nexthdr icmpv6 accept
+#
+#    tcp dport 67 tcp sport 68 accept
+#    tcp dport { 22, 53, 80, 443, 873, 2200 } accept
+#    udp dport 53 accept
+#
+#    ct state new log level warn prefix "egress attempt: "
+#    counter reject with icmp type admin-prohibited
+#  }
+#}
+#EOF
+#
+#sudo systemctl enable nftables.service
+#sudo systemctl start nftables.service
