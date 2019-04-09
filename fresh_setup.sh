df2677044abb55e0a84fd57ef41c2274ee6a86f0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-04-09 16:23:29 -0400

    Add in gdb enhancement

--- fresh_setup.sh
+++ fresh_setup.sh
@@ -91,6 +91,10 @@ rustup target add --toolchain nightly thumbv6m-none-eabi
 cargo install cargo-binutils itm
 rustup component add llvm-tools-preview
 
+# Install and setup GEF
+pip3 install --user unicorn capstone ropper keystone-engine
+wget -q -O- https://github.com/hugsy/gef/raw/master/scripts/gef.sh | sh
+
 # RVM gpg key
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
