5dc1f6354351e7cf7e554e0940c54248d52c73ee
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-11-21 22:21:39 -0600

    fix rust, it should be a user script not a root one

--- setup-scripts/rust.sh
+++ setup-scripts/rust.sh
@@ -2,8 +2,8 @@
 
 set -o errexit
 
-if [ ${EUID} != 0 ]; then
-  echo "This setup script is expecting to run as root."
+if [ ${EUID} = 0 ]; then
+  echo "This setup script is expecting to run as a regular user."
   exit 1
 fi
 
