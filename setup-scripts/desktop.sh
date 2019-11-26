257f54af64e8b70b81a0d75bf2354cf2df902a97
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-11-26 11:03:02 -0600

    add libreoffice to my basic desktop packages

--- setup-scripts/desktop.sh
+++ setup-scripts/desktop.sh
@@ -13,8 +13,8 @@ dnf install \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
-dnf install aircrack-ng awscli cheese fswebcam pcsc-lite-ccid pinentry-gtk \
-  privoxy tor transmission-gtk vlc wireshark youtube-dl -y
+dnf install aircrack-ng awscli cheese fswebcam libreoffice pcsc-lite-ccid \
+  pinentry-gtk privoxy tor transmission-gtk vlc wireshark youtube-dl -y
 
 systemctl enable pcscd.service
 systemctl start pcscd.service
