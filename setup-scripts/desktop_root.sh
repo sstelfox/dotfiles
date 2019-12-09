44af5cb34bc895d5704d4ec849ec17a8f26a8852
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-12-09 15:31:35 -0600

    yeah xclip should be a core utility...

--- setup-scripts/desktop_root.sh
+++ setup-scripts/desktop_root.sh
@@ -14,7 +14,7 @@ dnf install \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 dnf install aircrack-ng awscli cheese fswebcam libreoffice pcsc-lite-ccid \
-  pinentry-gtk privoxy tor transmission-gtk vlc wireshark youtube-dl -y
+  pinentry-gtk privoxy tor transmission-gtk vlc wireshark xclip youtube-dl -y
 
 systemctl enable pcscd.service
 systemctl start pcscd.service
