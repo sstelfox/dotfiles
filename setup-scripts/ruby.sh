d488a9edc7fc9890f3d2ef554f7175865b33e85e
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-12-09 16:27:58 -0600

    some more ruby setup in my setup scripts

--- setup-scripts/ruby.sh
+++ setup-scripts/ruby.sh
@@ -10,3 +10,9 @@ fi
 # RVM gpg key
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+# Re-source our bash profile so we can use RVM
+source ~/.bashrc
+
+# Install the globally available gems
+gem install pry
