6b907d6b8e6a1fd1b09f5c793b7417dc42f5988a
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2020-01-29 09:31:40 -0600

    move the SSH config regeneration into its own script

---
+++ ssh/build_config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o errtrace
+set -o pipefail
+set -o noclobber
+set -o nounset
+
+DOTFILE_DIR="$HOME/.dotfiles"
+
+# Generate our SSH config
+truncate -s 0 ${DOTFILE_DIR}/ssh/config
+
+for cfg_segment in $(ls $DOTFILE_DIR/ssh/shared-config/* 2>/dev/null | sort); do
+  cat ${cfg_segment} >> ${DOTFILE_DIR}/ssh/config
+  echo >> ${DOTFILE_DIR}/ssh/config
+done
+
+for cfg_segment in $(ls $DOTFILE_DIR/ssh/system-config/* 2>/dev/null | sort); do
+  cat ${cfg_segment} >> ${DOTFILE_DIR}/ssh/config
+  echo >> ${DOTFILE_DIR}/ssh/config
+done
+
+# Ensure our permissions are good
+chmod -R u=rwX,g=rX,o= ${DOTFILE_DIR}
+chmod -R u=rwX,g=,o= ${DOTFILE_DIR}/ssh
