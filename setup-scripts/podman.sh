03a65b3ad22317d7ec27459434ef42aa304a6221
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2022-05-13 14:13:11 -0500

    note on podman bits

--- setup-scripts/podman.sh
+++ setup-scripts/podman.sh
@@ -8,3 +8,14 @@ if [ ${EUID} != 0 ]; then
 fi
 
 dnf install buildah podman -y
+
+# Note to self: I also rely on a subtle change to the podman network config
+# file located at `/etc/cni/net.d/87-podman-bridge.conflist`. Specifically I
+# add the following plugin to allow local dns resolution of other pods.
+#
+# ```
+#     {
+#       "type": "dnsname",
+#       "domainName": "podman.local",
+#     },
+# ```
