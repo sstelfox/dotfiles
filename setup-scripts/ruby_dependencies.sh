bd61a867f52906a73fe74106e5e236dcd4ec22fa
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-01-20 15:41:07 -0500

    minor tweaks to standard config

--- setup-scripts/ruby_dependencies.sh
+++ setup-scripts/ruby_dependencies.sh
@@ -14,5 +14,5 @@ fi
 # Note: some tests and environments I have require the redis client which is
 # available in the redis package.
 
-dnf install autoconf automake bison gcc-c++ libffi-devel libpq-devel libtool \
+dnf install autoconf automake bison gcc-c++ libffi-devel libpq-devel libstdc++-static libtool \
   libyaml-devel patch readline-devel sqlite-devel zlib-devel -y
