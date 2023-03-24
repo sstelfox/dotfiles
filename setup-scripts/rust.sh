5d1c781fa8f1f8755eed87585e27ac51b3af1226
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-03-24 09:14:12 -0400

    fix a bug in rust setup

--- setup-scripts/rust.sh
+++ setup-scripts/rust.sh
@@ -3,19 +3,19 @@
 set -o errexit
 
 if [ ${EUID} = 0 ]; then
-  echo "This setup script is expecting to run as a regular user."
-  exit 1
+	echo "This setup script is expecting to run as a regular user."
+	exit 1
 fi
 
 if [ ! -f $HOME/.cargo/env ]; then
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path --default-toolchain nightly -y
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path --default-toolchain nightly -y
 fi
 
 source $HOME/.cargo/env
 
 rustup component add rust-analyzer
 rustup component add rustfmt
-rustup component add rustsrc
+rustup component add rust-src
 rustup component add clippy
 rustup component add miri
 
