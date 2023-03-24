a2cd785b6f7012ecf3b1f546fe4a49d3f623a90c
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-03-24 14:43:53 -0400

    lazy doesn't work for me, but I want some of it so back to the manual port

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,18 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+--[[ init.lua ]]
+
+-- LEADER
+--
+-- These keybindings need to be defined before the first / is called;
+-- otherwise, it will default to "\"
+
+-- vim.g.mapleader = ","
+-- vim.g.localleader = "\\"
+vim.g.mapleader = " "
+vim.g.localleader = " "
+
+-- IMPORTS
+require('vars')      -- Variables
+require('opts')      -- Options
+require('keys')      -- Keymaps
+require('plugins')   -- Plugins
+require('fixes')     -- Fixes for various weird behaviors
