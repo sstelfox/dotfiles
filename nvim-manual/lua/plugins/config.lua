e3ce4ca07dfa17cb21720516453dee39ce7484b0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-03-27 11:38:46 -0400

    fix complaint when plugins aren't present

--- nvim-manual/lua/plugins/config.lua
+++ nvim-manual/lua/plugins/config.lua
@@ -1,6 +1,14 @@
+function safe_setup(plugin_name, opts)
+    if packer_plugins[plugin_name] and packer_plugins[plugin_name].loaded then
+        return require(plugin_name).setup(opts)
+    end
+
+    return {}
+end
+
 -- Setup Mason language server
-require('mason').setup({})
-require('mason-lspconfig').setup()
+safe_setup('mason')
+safe_setup('mason-lspconfig')
 
 -- Setup rust tooling
-require('rust-tools').setup()
+safe_setup('rust-tools')
