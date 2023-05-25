f0e11bc6ca7f0a12606f76e88239bba7613a73b8
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-05-25 12:34:11 -0400

    tweaks

--- setup-scripts/desktop_root.sh
+++ setup-scripts/desktop_root.sh
@@ -3,20 +3,23 @@
 set -o errexit
 
 if [ ${EUID} != 0 ]; then
-  echo "This setup script is expecting to run as root."
-  exit 1
+	echo "This setup script is expecting to run as root."
+	exit 1
 fi
 
 # A lot of my desktop specific software requires repos outside of the core, the
 # RPM fusion repos handle that for me
 if [ ! -f /etc/yum.repos.d/rpmfusion-free.repo ]; then
-  dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+	dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 fi
 
 if [ ! -f /etc/yum.repos.d/rpmfusion-nonfree.repo ]; then
-  dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+	dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 fi
+
+# should add in a generic development dependencies script
+dnf install clang cmake -y
 
 # fuse is required to run appimages like Obsidian
 dnf install cheese discord fswebcam fuse libreoffice screen strace \
-  transmission-gtk vlc xclip youtube-dl -y
+	transmission-gtk vlc xclip youtube-dl -y
