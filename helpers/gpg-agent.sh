be6b64c16ef97a650452f64cb398a33b5b126812
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-09-20 16:44:58 -0400

    fix: independent ssh agent access in unique shells

--- helpers/gpg-agent.sh
+++ helpers/gpg-agent.sh
@@ -3,11 +3,10 @@
 # Only attempt to setup the gpg-agent if we're not on an SSH connection
 if [ -z "${SSH_CONNECTION}" ]; then
   unset SSH_AGENT_PID
-
-  if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-  fi
-
   export GPG_TTY=$(tty)
   gpg-connect-agent updatestartuptty /bye >/dev/null 3>&1
+fi
+
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
