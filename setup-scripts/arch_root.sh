41950a5d5bdafec7214a3cc70c21a4f4474a54a4
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-11-04 10:20:06 -0400

    feat: additional setup scripts

---
+++ setup-scripts/arch_root.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+
+if [ ${EUID} != 0 ]; then
+  echo "This setup script should only be run by root."
+  exit 1
+fi
+
+pacman -Sy wireguard-tools nftables podman tcpdump git-lfs graphviz iotop \
+  git-crypt jq ripgrep blender inkscape gimp krita zopfli ansible discord \
+  libreoffice-fresh screen strace vlc xclip transmission-qt yt-dlp clang \
+  cmake nvidia-prime nvidia-utils rsync nvidia-dkms linux-hardened-headers
