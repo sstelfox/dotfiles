e3c20ebb0f3546bc9ab371cf1a0a7227036fa8a5
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-11-06 23:28:23 -0500

    feat: more arch tweaks

--- setup-scripts/arch_root.sh
+++ setup-scripts/arch_root.sh
@@ -10,4 +10,8 @@ fi
 pacman -Sy wireguard-tools nftables podman tcpdump git-lfs graphviz iotop \
   git-crypt jq ripgrep blender inkscape gimp krita zopfli ansible discord \
   libreoffice-fresh screen strace vlc xclip transmission-qt yt-dlp clang \
-  cmake nvidia-prime nvidia-utils rsync nvidia-dkms linux-hardened-headers
+  cmake nvidia-prime nvidia-utils rsync nvidia-dkms linux-hardened-headers \
+  bluez bluez-utils
+
+systemctl start bluetooth.service
+systemctl enable bluetooth.service
