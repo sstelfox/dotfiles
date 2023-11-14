143c95b8dec4fcf471acb479605669e871c4f847
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-11-13 22:20:29 -0500

    feat: little bit of refinement

---
+++ nvim/lua/plugins/ra-mux.lua
@@ -0,0 +1,11 @@
+--require("lspconfig")["rust_analyzer"].setup({
+--  cmd = vim.lsp.rpc.connect("127.0.0.1", 27631),
+--})
+
+-- init_options = {
+--   lspMux = {
+--     version = "1",
+--     method = "connect",
+--     server = "rust-analyzer",
+--   },
+-- },
