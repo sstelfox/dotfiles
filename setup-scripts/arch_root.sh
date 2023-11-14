9cbd04058475c9310ad78ef189d89d9c23048f70
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-11-14 08:02:59 -0500

    feat: more arch pkgs

--- setup-scripts/arch_root.sh
+++ setup-scripts/arch_root.sh
@@ -3,18 +3,24 @@
 set -o errexit
 
 if [ ${EUID} != 0 ]; then
-  echo "This setup script should only be run by root."
-  exit 1
+	echo "This setup script should only be run by root."
+	exit 1
 fi
 
-pacman -Sy wireguard-tools nftables podman tcpdump git-lfs graphviz iotop \
-  git-crypt jq ripgrep blender inkscape gimp krita zopfli ansible discord \
-  libreoffice-fresh screen strace vlc xclip transmission-qt yt-dlp clang \
-  cmake nvidia-prime nvidia-utils rsync nvidia-dkms linux-hardened-headers \
-  bluez bluez-utils fuse2 okular fd
+pacman -Syy wireguard-tools nftables podman tcpdump git-lfs graphviz iotop \
+	git-crypt jq ripgrep blender inkscape gimp krita zopfli ansible discord \
+	libreoffice-fresh screen strace vlc xclip transmission-qt yt-dlp clang \
+	cmake nvidia-prime nvidia-utils rsync nvidia-dkms linux-hardened-headers \
+	bluez bluez-utils fuse2 okular fd
+
+# Secure boot setup
+pacman -Syy sbctl
+
+# For podman DNS networking
+pacman -Syy aardvark-dns podman-dnsname
 
 # This package seemed to massively improve the "lag" I felt on the work laptop
-pacman -Sy xf86-video-amdgpu
+pacman -Syy xf86-video-amdgpu
 
 systemctl start bluetooth.service
 systemctl enable bluetooth.service
