5f15359a7d5e84df9a99a0283f9cca386e22ad1d
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-01-09 12:34:18 -0500

    feat: stripe cli installation, some random uncommited config changes

---
+++ setup-scripts/stripe_user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+
+if [ ${EUID} = 0 ]; then
+	echo "This setup script is expecting to run as a regular user."
+	exit 1
+fi
+
+wget -O /tmp/strip.tar.gz https://github.com/stripe/stripe-cli/releases/download/v1.19.1/stripe_1.19.1_linux_x86_64.tar.gz
+tar -xf /tmp/strip.tar.gz -C $HOME/.dotfiles/in_path/bin stripe
+rm -f /tmp/stripe.tar.gz
