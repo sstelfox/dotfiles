a86af4dd6bc5a4807ad3cd03573d814a3a14823a
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-01-28 09:41:06 -0500

    fix: consistent lua string usage

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,12 @@
 -- vim: set ts=2 sw=2 expandtab ai :
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap the package manager
   vim.fn.system({
-    "git", "clone", "--filter=blob:none", "--branch=stable",
-    "https://github.com/folke/lazy.nvim.git", lazypath
+    'git', 'clone', '--filter=blob:none', '--branch=stable',
+    'https://github.com/folke/lazy.nvim.git', lazypath
   })
 end
 
@@ -15,12 +15,12 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 -- Some of these options need to be configured before we start sourcing plugin
 -- modules to ensure they're correctly applied, such as mapleader and
 -- maplocalleader.
-require("options")
+require('options')
 
-require("autocmds")
-require("keymaps")
+require('autocmds')
+require('keymaps')
 
-require("lazy").setup({
+require('lazy').setup({
   -- If our neovim config gets changed, load it up automatically in open
   -- sessions and let us know that happened. Unlike plugin updates, these are
   -- actions we've taken and want our editor to reflect them.
@@ -55,7 +55,7 @@ require("lazy").setup({
 
     -- During start-up lazy may be missing some number of plugins, if it does it
     -- will attempt to switch to these color schemes in order if they're present.
-    colorscheme = { "tokyonight" },
+    colorscheme = { 'tokyonight' },
   },
 
   performance = {
