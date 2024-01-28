a3fde670bec2667e445cd6073b2a835917a88e46
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-01-28 16:49:45 -0500

    fix: point at the local lazyvim so I can begin gutting it

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,34 @@
 -- vim: set ts=2 sw=2 expandtab ai :
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
 	-- bootstrap the package manager
 	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--branch=stable",
-		"https://github.com/folke/lazy.nvim.git",
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'--branch=stable',
+		'https://github.com/folke/lazy.nvim.git',
 		lazypath,
 	})
 end
 
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
-require("lazy").setup({
+-- Define all the plugins I want to use and their specific configurations, this
+-- doesn't necessarily load them
+local lazy_spec = {
+  -- Still very not happy about quite a few of LazyVim's options, I've decided
+  -- to inline it and gut it freely down to its minimums until I'm happy with
+  -- the results.
+  { 'LazyVim/LazyVim', import = 'lazyvim.plugins', dev = true },
+
+  -- Include my own plugin systems
+  { import = 'plugins' },
+}
+
+local lazy_settings = {
 	-- If our neovim config gets changed, load it up automatically in open
 	-- sessions and let us know that happened. Unlike plugin updates, these are
 	-- actions we've taken and want our editor to reflect them.
@@ -44,6 +56,10 @@ require("lazy").setup({
 		version = false,
 	},
 
+  dev = {
+    path = '~/.dotfiles/nvim/dev',
+  },
+
 	install = {
 		-- Install missing plugins on startup, if they're already present this
 		-- doesn't increase startup time.
@@ -51,7 +67,7 @@ require("lazy").setup({
 
 		-- During start-up lazy may be missing some number of plugins, if it does it
 		-- will attempt to switch to these color schemes in order if they're present.
-		colorscheme = { "tokyonight" },
+		colorscheme = { 'tokyonight' },
 	},
 
 	performance = {
@@ -70,25 +86,17 @@ require("lazy").setup({
 
 			-- Disable various run time path plugins for performance
 			disabled_plugins = {
-				"gzip",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
+				'gzip',
+				'tarPlugin',
+				'tohtml',
+				'tutor',
+				'zipPlugin',
 			},
 		},
 	},
 
-	-- Define all the plugins I want to use and their specific configurations,
-	-- this doesn't necessarily load them
-	spec = {
-		-- Still very not happy about quite a few of LazyVim's options, I've decided
-		-- to inline it and gut it freely down to its minimums until I'm happy with
-		-- the results.
-		{ import = "lazyvim" },
-		{ import = "lazyvim.plugins" },
+  -- This is passed separately
+	spec = null,
+}
 
-		-- Include my own plugin systems
-		{ import = "plugins" },
-	},
-})
+require('lazy').setup(lazy_spec, lazy_settings)
