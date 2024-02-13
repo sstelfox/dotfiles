8fcd62eec01d7d3e035372bb08fe395b6eb8b6cc
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-02-13 12:02:02 -0500

    fix: work laptop is broken

---
+++ setup-scripts/arch_linux/interactive/displaylink.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+
+if [ ${EUID} = 0 ]; then
+	echo "This setup script is expecting to run as a regular user with sudo permissions"
+	exit 1
+fi
+
+if ! which yay &>/dev/null; then
+	echo 'yay needs to be installed to install this AUR package'
+	exit 1
+fi
+
+sudo pacman -Sy --needed --noconfirm linux-headers linux-hardened-headers
+
+yay -S evdi-git
+yay -S displaylink
+
+sudo systemctl enable displaylink.service
+
+cat <<EOF | sudo tee /etc/X11/xorg.conf.d/20-evdi.conf
+Section "OutputClass"
+	Identifier "DisplayLink"
+	MatchDriver "evdi"
+	Driver "modesetting"
+	Option "AccelMethod" "none"
+EndSection
+EOF
--- setup-scripts/arch_linux/root/nodejs_js_tsx.sh
+++ setup-scripts/arch_linux/root/nodejs_js_tsx.sh
@@ -7,4 +7,4 @@ if [ ${EUID} != 0 ]; then
 	exit 1
 fi
 
-pacman -Sy --noconfirm nodejs npm typescript yarn
+pacman -Sy --needed --no-confirm nodejs npm typescript yarn
