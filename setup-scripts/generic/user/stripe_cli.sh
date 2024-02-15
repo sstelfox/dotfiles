26164e35e26ec4a1f37dce8b25249c330d3dd01c
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-02-15 09:13:27 -0500

    fix: typo in install script

--- setup-scripts/generic/user/stripe_cli.sh
+++ setup-scripts/generic/user/stripe_cli.sh
@@ -7,6 +7,6 @@ if [ ${EUID} = 0 ]; then
 	exit 1
 fi
 
-wget -O /tmp/strip.tar.gz https://github.com/stripe/stripe-cli/releases/download/v1.19.1/stripe_1.19.1_linux_x86_64.tar.gz
-tar -xf /tmp/strip.tar.gz -C $HOME/.dotfiles/in_path/bin stripe
+wget -O /tmp/stripe.tar.gz https://github.com/stripe/stripe-cli/releases/download/v1.19.1/stripe_1.19.1_linux_x86_64.tar.gz
+tar -xf /tmp/stripe.tar.gz -C $HOME/.dotfiles/in_path/bin stripe
 rm -f /tmp/stripe.tar.gz
