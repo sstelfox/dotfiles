6a96849995419ab265ae4496bdbc1ebc7ec5c781
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-05-21 10:58:21 -0400

    feat: add vpn bits for hosting

--- setup-scripts/generic/interactive/ollama.sh
+++ setup-scripts/generic/interactive/ollama.sh
@@ -15,6 +15,11 @@ cat <<EOF | sudo tee /etc/systemd/system/ollama.service.d/cors.conf
 Environment="OLLAMA_ORIGINS=*"
 EOF
 
+cat <<EOF | sudo tee /etc/systemd/system/ollama.service.d/vpn_hosting.conf.dis
+[Service]
+Environment="OLLAMA_HOST=172.16.50.3:11434"
+EOF
+
 sudo systemctl daemon-reload
 sudo systemctl enable ollama.service
 sudo systemctl restart ollama.service
