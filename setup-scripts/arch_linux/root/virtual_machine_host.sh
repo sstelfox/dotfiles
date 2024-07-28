dc4eea1fe51564e730ae8a13c92da88b88b3d85e
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-07-28 13:33:17 -0400

    feat: some install scripts

---
+++ setup-scripts/arch_linux/root/virtual_machine_host.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+
+if [ ${EUID} != 0 ]; then
+  echo "This setup script should only be run by root."
+  exit 1
+fi
+
+pacman -Sy --noconfirm --needed edk2-ovmf libvirt qemu-desktop swtpm virt-manager
+
+systemctl enable libvirtd.service
