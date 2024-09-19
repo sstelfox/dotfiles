e9e57e60d9e8b1d93fcd9a33bf0e2a5328796e1c
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-09-18 21:25:52 -0400

    feat: random modifications

--- setup-scripts/arch_linux/root/yay.sh
+++ setup-scripts/arch_linux/root/yay.sh
@@ -3,15 +3,19 @@
 set -o errexit
 
 if [ ${EUID} != 0 ]; then
-	echo "This setup script should only be run by root."
-	exit 1
+  echo "This setup script should only be run by root."
+  exit 1
 fi
 
 if ! which -q git &>/dev/null; then
-	echo 'git is needed to install yay'
-	exit 1
+  echo 'git is needed to install yay'
+  exit 1
 fi
 
+# Silent dependencies I may not have installed (such as my minimal server environments)
+pacman --asdeps -S --needed --noconfirm debugedit fakeroot gcc make
+
+# Direct dependencies of yay
 pacman --asdeps -S --needed --noconfirm 'go>=1.19'
 
 rm -rf /tmp/yay-build
@@ -25,14 +29,14 @@ su -s /bin/bash --pty nobody sh -c '(
 )'
 
 (
-	cd /tmp/yay-build/yay
-	export GOCACHE=$(pwd)/go-cache
-	runuser -unobody -- makepkg -c
+  cd /tmp/yay-build/yay
+  export GOCACHE=$(pwd)/go-cache
+  runuser -unobody -- makepkg -c
 )
 
 (
-	cd /tmp/yay-build/yay
-	pacman -U --noconfirm yay-*-x86_64.pkg.tar.zst
+  cd /tmp/yay-build/yay
+  pacman -U --noconfirm yay-*-x86_64.pkg.tar.zst
 )
 
 rm -rf /tmp/yay-build
