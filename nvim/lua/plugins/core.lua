519fd8db318af01c3c5ecd66fdaea0f84806c233
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-10-27 00:15:01 -0400

    feat: fix more of my neovim environment

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -2,7 +2,7 @@ return {
 	-- Still very not happy about quite a few of LazyVim's options, I've decided
 	-- to inline it and gut it freely down to its minimums until I'm happy with
 	-- the results.
-	{ "LazyVim/LazyVim", import = "lazyvim.plugins", dev = true },
+	--{ "LazyVim/LazyVim", import = "lazyvim.plugins", dev = true },
 
 	-- Include all my automatically loaded plugin configs
 	{ import = "plugins/autoload" },
