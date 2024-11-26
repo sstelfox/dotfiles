b7b96712ddaff5ff805778841b0031105e1d2df6
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-11-25 19:24:51 -0500

    feat/fix: add lfs support, fix whitespace in setup script

--- system-setup-scripts/generic/user/rust.sh
+++ system-setup-scripts/generic/user/rust.sh
@@ -3,17 +3,17 @@
 set -o errexit
 
 if [ ${EUID} = 0 ]; then
-	echo "This setup script is expecting to run as a regular user."
-	exit 1
+  echo "This setup script is expecting to run as a regular user."
+  exit 1
 fi
 
 if ! which cmake &>/dev/null; then
-	echo 'cmake is needed for some rust dependencies to be built'
-	exit 1
+  echo 'cmake is needed for some rust dependencies to be built'
+  exit 1
 fi
 
 if [ ! -f $HOME/.cargo/env ]; then
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path --default-toolchain nightly -y
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path --default-toolchain nightly -y
 fi
 
 source $HOME/.cargo/env
@@ -41,17 +41,17 @@ cat <<EOF >$HOME/.cargo/config.toml
 [build]
 rustc-wrapper = "sccache"
 
-[target.x86_64-unknown-linux-gnu]
-linker = "clang"
-rustflags = ["-C", "link-arg=-fuse-ld=/sbin/mold"]
+#[target.x86_64-unknown-linux-gnu]
+#linker = "clang"
+#rustflags = ["-C", "link-arg=-fuse-ld=/sbin/mold"]
 EOF
 
 if [ "${EMBEDDED_DEVELOPMENT}" = "y" ]; then
-	rustup target add --toolchain stable thumbv6m-none-eabi
-	rustup target add --toolchain nightly thumbv6m-none-eabi
+  rustup target add --toolchain stable thumbv6m-none-eabi
+  rustup target add --toolchain nightly thumbv6m-none-eabi
 
-	cargo install cargo-binutils itm
-	rustup component add llvm-tools-preview
+  cargo install cargo-binutils itm
+  rustup component add llvm-tools-preview
 fi
 
 mkdir -p ~/workspace/rust
