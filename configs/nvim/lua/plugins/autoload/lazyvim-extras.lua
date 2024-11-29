7beb44d28d0ca6a7a0895ec7c31755fb8081741e
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-11-29 11:53:08 -0500

    fix: update nvim deps and disable broken copilot

--- configs/nvim/lua/plugins/autoload/lazyvim-extras.lua
+++ configs/nvim/lua/plugins/autoload/lazyvim-extras.lua
@@ -1,8 +1,8 @@
 return {
-	{ import = "lazyvim.plugins.extras.coding.copilot", opt = true },
-	{ import = "lazyvim.plugins.extras.lang.markdown", opt = true },
-	{ import = "lazyvim.plugins.extras.lang.python" },
-	{ import = "lazyvim.plugins.extras.lang.rust", opt = true },
-	{ import = "lazyvim.plugins.extras.lang.typescript" },
-	{ import = "lazyvim.plugins.extras.lang.yaml" },
+  --{ import = "lazyvim.plugins.extras.coding.copilot", opt = true },
+  { import = "lazyvim.plugins.extras.lang.markdown", opt = true },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust", opt = true },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
 }
