f1e7bfe84ef58829766e23dfbf9734c5d1e1d140
Your Name <you@example.com>
2025-02-09 18:08:24 -0500

    fix: more shellcheck / posix fixes

--- containers/inner-build.sh
+++ containers/inner-build.sh
@@ -5,6 +5,6 @@ set -o nounset
 
 CONTAINER_NAME="${1}"
 
-nix-build ./nix-definitions/${CONTAINER_NAME}.nix -A exporter
-./result/bin/export-${CONTAINER_NAME}
+nix-build ./nix-definitions/"${CONTAINER_NAME}".nix -A exporter
+./result/bin/export-"${CONTAINER_NAME}"
 rm -rf result
