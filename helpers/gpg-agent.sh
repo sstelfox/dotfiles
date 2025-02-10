8ba12fb34d71192b46e9073f46aa7521fbb4a59f
Your Name <you@example.com>
2025-02-09 20:33:46 -0500

    fix: don't start ssh agents when on an ssh connection

--- helpers/gpg-agent.sh
+++ helpers/gpg-agent.sh
@@ -3,10 +3,12 @@
 # Only attempt to setup the gpg-agent if we're not on an SSH connection
 if [ -z "${SSH_CONNECTION}" ]; then
   unset SSH_AGENT_PID
+  # shellcheck disable=SC2155
   export GPG_TTY="$(tty)"
-  gpg-connect-agent updatestartuptty /bye >/dev/null 3>&1
+  gpg-connect-agent updatestartuptty /bye >/dev/null >/dev/null 2>&1
 fi
 
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+  # shellcheck disable=SC2155
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket 2>/dev/null)"
 fi
