eb4b60809de4855064a1dc2604985c370c880006
Your Name <you@example.com>
2025-02-11 18:55:34 -0500

    feat: I think that's more compatible, it has fewer shellcheck warnings at the very least

--- helpers/shortdir.sh
+++ helpers/shortdir.sh
@@ -1,54 +1,110 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-begin="" # The unshortened beginning of the path.
-shortbegin="" # The shortened beginning of the path.
-current="" # The section of the path we're currently working on.
-end="${2:-$(pwd)}/" # The unmodified rest of the path.
+set -o errexit
+set -o nounset
 
-if [[ "$end" =~ "$HOME" ]]; then
-    INHOME=1
-    end="${end#$HOME}" #strip /home/username from start of string
-    begin="$HOME"      #start expansion from the right spot
-else
-    INHOME=0
-fi
+is_unique_prefix() {
+  dir="${1}"
+  prefix="${2}"
+  current="${3}" # Add current as a parameter to verify exact match
 
-end="${end#/}" # Strip the first /
-shortenedpath="$end" # The whole path, to check the length.
+  # Escape special characters for grep
+  escaped_prefix="$(printf "%s" "${prefix}" | sed 's/[]\[\^$.*]/\\&/g')"
+
+  # Use ls -A to show hidden files, but not . and use grep to find matches that start with our
+  # prefix
+  matches="$(ls -A1 "${dir}" 2>/dev/null | grep "^${escaped_prefix}" || true)"
+
+  # Count the matches
+  match_count="$(printf "%s\n" "${matches}" | grep -c '^' || true)"
+
+  # If we have exactly one match, verify it's the one we want
+  if [ "${match_count}" -eq 1 ]; then
+    # The match should exactly equal our current component
+    [ "${matches}" = "${current}" ]
+    return $?
+  fi
+
+  return 1
+}
+
+strlen() {
+  expr length "${1}"
+}
+
+substr_start() {
+  str="${1}"
+  len="${2}"
+
+  expr substr "${str}" 1 "${len}"
+}
+
+begin=""
+shortbegin=""
+current=""
+
+case "${2:-}" in
+"") end="$(pwd)/" ;;
+*) end="${2}/" ;;
+esac
+
+case "${end}" in
+"${HOME}"*)
+  INHOME=1
+  end="${end#"${HOME}"}"
+  begin="${HOME}"
+  ;;
+*)
+  INHOME=0
+  ;;
+esac
+
+end="${end#/}"
+shortenedpath="${end}"
 maxlength="${1:-0}"
 
-shopt -q nullglob && NGV="-s" || NGV="-u" # Store the value for later.
-shopt -s nullglob    # Without this, anything that doesn't exist in the filesystem turns into */*/*/...
+# shellcheck disable=SC2310
+while [ -n "${end}" ] && [ "$(strlen "${shortenedpath}" || true)" -gt "${maxlength}" ]; do
+  current="${end%%/*}"
+  end="${end#*/}"
 
-while [[ "$end" ]] && (( ${#shortenedpath} > maxlength ))
-do
-  current="${end%%/*}" # everything before the first /
-  end="${end#*/}"    # everything after the first /
+  shortcur="${current}"
+  shortcurstar="${current}"
 
-  shortcur="$current"
-  shortcurstar="$current" # No star if we don't shorten it.
+  curlen="$(strlen "${current}")"
+  i="$((curlen - 2))"
 
-  for ((i=${#current}-2; i>=0; i--)); do
-    subcurrent="${current:0:i}"
-    matching=("$begin/$subcurrent"*) # Array of all files that start with $subcurrent. 
-    (( ${#matching[*]} != 1 )) && break # Stop shortening if more than one file matches.
-    shortcur="$subcurrent"
-    shortcurstar="$subcurrent*"
+  while [ "${i}" -ge 0 ]; do
+    # Only try shortening if the current component is long enough
+    if [ "${i}" -gt 0 ]; then
+      subcurrent="$(substr_start "${current}" "${i}")"
+
+      # shellcheck disable=SC2310 # desired behavior
+      if is_unique_prefix "${begin}" "${subcurrent}" "${current}"; then
+        # shellcheck disable=SC2034 # false detection
+        shortcur="${subcurrent}"
+
+        # shellcheck disable=SC2034 # false detection
+        shortcurstar="${subcurrent}*"
+      else
+        break
+      fi
+    fi
+
+    i="$((i - 1))"
   done
 
-  #advance
-  begin="$begin/$current"
-  shortbegin="$shortbegin/$shortcurstar"
-  shortenedpath="$shortbegin/$end"
+  begin="${begin}/${current}"
+  shortbegin="${shortbegin}/${shortcurstar}"
+  shortenedpath="${shortbegin}/${end}"
 done
 
-shortenedpath="${shortenedpath%/}" # strip trailing /
-shortenedpath="${shortenedpath#/}" # strip leading /
+shortenedpath="${shortenedpath%/}"
+shortenedpath="${shortenedpath#/}"
 
-if [ $INHOME -eq 1 ]; then
-  echo "~/$shortenedpath" #make sure it starts with ~/
+if [ "${INHOME}" -eq 1 ]; then
+  # shellcheck disable=SC2088
+  printf "~/%s\n" "${shortenedpath}"
 else
-  echo "/$shortenedpath" # Make sure it starts with /
+  printf "/%s\n" "${shortenedpath}"
 fi
-
-shopt "$NGV" nullglob # Reset nullglob in case this is being used as a function.
