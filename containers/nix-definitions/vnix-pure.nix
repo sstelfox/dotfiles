82e5572c339dda4026d6b5383a78df887f0c4033
Your Name <you@example.com>
2025-02-23 00:06:38 -0500

    feat: some work towards a better vnix

--- containers/nix-definitions/vnix-pure.nix
+++ containers/nix-definitions/vnix-pure.nix
@@ -8,26 +8,60 @@
 }:
 
 let
+  inherit (pkgs) lib;
   imageName = "vnix-pure";
   imageTag = nixVersion;
 
-  env = pkgs.buildEnv {
-    name = "image-root";
-    paths = [
-      pkgs.bashInteractive
-      pkgs.coreutils
-      pkgs.nix
-    ];
+  # Where I left this off... Claude can't figure this out, the correct answer is to look up the
+  # definition of the package being called with a name like a tarball in the nixpkgs repo
+  baseSystem = lib.nixosSystem {
+    system = pkgs.system;
+    modules = [({ config, pkgs, ... }: {
+      imports = [
+        "${pkgs.path}/nixos/modules/installer/cd-dvd/channel.nix"
+        "${pkgs.path}/nixos/modules/profiles/minimal.nix"
+        "${pkgs.path}/nixos/modules/profiles/clone-config.nix"
+      ];
+
+      boot.isContainer = true;
+
+      environment.systemPackages = [
+        config.nix.package
+      ];
+
+      boot.postBootCommands = ''
+        if [ -f /nix-path-registration ]; then
+          ${config.nix.package.out}/bin/nix-store --load-db < /nix-path-registration &&
+            rm /nix-path-registration
+        fi
+
+        ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
+      '';
+
+      system.activationScripts.installInitScript = ''
+        ln -sf $systemConfig/init /init
+      '';
+    })];
   };
 
   containerImage = pkgs.dockerTools.buildImage {
     name = "stelfox.net/containers/${imageName}";
     tag = imageTag;
 
-    copyToRoot = env;
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      paths = [
+        baseSystem.config.system.build.toplevel
+        pkgs.stdenv
+      ];
+    };
+
+    extraCommands = ''
+      mkdir -p proc sys dev etc
+    '';
 
     config = {
-      Cmd = [ "/bin/bash" ];
+      Cmd = [ "/init" ];
       WorkingDir = "/workspace";
     };
   };
@@ -44,11 +78,11 @@ let
     cp "${containerImage}" "$OUT_FILE"
     echo "image written to: $OUT_FILE"
   '';
-in
-  {
-    inherit containerImage;
-    exporter = exportScript;
 
-    # Allow others to use this if they need it
-    inherit imageName imageTag;
-  }
+in {
+  inherit containerImage;
+  exporter = exportScript;
+
+  # Allow others to use this if they need it
+  inherit imageName imageTag;
+}
