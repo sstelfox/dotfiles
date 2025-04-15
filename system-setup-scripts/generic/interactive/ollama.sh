3b3277db2e05fa09863eb5f1e1b57bb0c6407844
Your Name <you@example.com>
2025-04-15 12:48:15 -0400

    feat: cut out bits of ollama that don't work well

--- system-setup-scripts/generic/interactive/ollama.sh
+++ system-setup-scripts/generic/interactive/ollama.sh
@@ -10,6 +10,10 @@ fi
 #curl -fsSL https://ollama.com/install.sh | sh
 pacman -Sy ollama-cuda
 
+# Add to fstab:
+# /var/run/media/sstelfox/TLPRed-TS+/language_models/ollama /var/lib/ollama/.ollama     none     defaults,bind,noauto     0 0
+# mount /var/lib/ollama/.ollama before starting
+
 sudo mkdir -p /etc/systemd/system/ollama.service.d
 cat <<EOF | sudo tee /etc/systemd/system/ollama.service.d/cors.conf
 [Service]
@@ -22,5 +26,6 @@ Environment="OLLAMA_HOST=172.16.50.3:11434"
 EOF
 
 sudo systemctl daemon-reload
-sudo systemctl enable ollama.service
+# Don't autostart, need to manually mount... Could have systemd handle it but *shrug*
+#sudo systemctl enable ollama.service
 sudo systemctl restart ollama.service
