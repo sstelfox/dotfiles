456cc96d8477876845e8e8055ba96ab5da737d48
Your Name <you@example.com>
2025-06-20 22:13:32 -0400

    feat: refine this

--- system-setup-scripts/arch_linux/root/app_images_and_containers_namespace_fix.sh
+++ system-setup-scripts/arch_linux/root/app_images_and_containers_namespace_fix.sh
@@ -3,10 +3,12 @@
 set -o errexit
 
 if [ ${EUID} != 0 ]; then
-	echo "This setup script should only be run by root."
-	exit 1
+  echo "This setup script should only be run by root."
+  exit 1
 fi
 
+# This is for steam
 cat <<EOF >/etc/sysctl.d/00-enable-unprivileged-userns.conf
 kernel.unprivileged_userns_clone=1
 EOF
+sysctl -w kernel.unprivileged_userns_clone=1
