e8280c5019509429a694688cfa2be5c2b9712f7d
Your Name <you@example.com>
2025-11-05 15:11:30 -0500

    fix: update my preferred ruby baseline 3.2.x version to the latest

--- system-setup-scripts/generic/user/ruby.sh
+++ system-setup-scripts/generic/user/ruby.sh
@@ -3,22 +3,22 @@
 set -o errexit
 
 if [ ${EUID} = 0 ]; then
-	echo "This setup script is expecting to run as a regular user."
-	exit 1
+  echo "This setup script is expecting to run as a regular user."
+  exit 1
 fi
 
 # RVM gpg key
 gpg2 --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys \
-	409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles stable
 
 # Re-source our bash profile so we can use RVM
 source /home/sstelfox/.rvm/scripts/rvm
 
-rvm install 3.2.2 --no-docs
+rvm install 3.2.8 --no-docs
 rvm gemset create global
 
-rvm use ruby-3.2.2@global --default
+rvm use ruby-3.2.8@global --default
 rvm gemset use global --default
 
 # Install the globally available gems
